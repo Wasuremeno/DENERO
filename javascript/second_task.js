05608ea22a7cd793e43e57e40179059d8035c26b
Wasuremeno <wasuremono19@gmail.com>
2026-02-07 16:23:19 +0300

    feat: add popup creation script

---
+++ javascript/second_task.js
@@ -0,0 +1,9 @@
+
+
+
+// function setGetParams(newGetparams) {
+// 	let newString
+// 	for(key in newGetParams)
+// 		newString += (newString ? '&' : '') + key + '=' + newGetParams[i]
+// 	return window.location.href + '?' + newString
+// }
