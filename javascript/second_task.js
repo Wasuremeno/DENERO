d3c75a67803047c230cd7db4642065a059ee20fe
Wasuremeno <wasuremono19@gmail.com>
2026-02-07 16:40:30 +0300

    fix: correct function param name and key access in setGetParams

--- javascript/second_task.js
+++ javascript/second_task.js
@@ -1,4 +1,4 @@
-
+// Задание 2
 
 
 // function setGetParams(newGetparams) {
@@ -7,3 +7,17 @@
 // 		newString += (newString ? '&' : '') + key + '=' + newGetParams[i]
 // 	return window.location.href + '?' + newString
 // }
+
+//newGetparams => newGetParams
+// 'newGetparams' is declared but its value is never read.ts(6133)
+//console не выдавала никаких errors
+//
+//есть личный вопрос, почему не TypeScript?
+//типизация параметра и в for in цикле перебора была бы заметно при компиляции
+
+function setGetParams(newGetParams) {
+  let newString = '';
+  for(let key in newGetParams)
+    newString += (newString ? '&' : '') + key + '=' + newGetParams[key];//i=>key
+  return window.location.href + '?' + newString;
+}
