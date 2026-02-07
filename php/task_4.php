bb44d1cc046a223970a71b3d923f184ad580b3d9
Wasuremeno <wasuremono19@gmail.com>
2026-02-07 18:25:52 +0300

    feat: Implement singleton pattern in extended class

---
+++ php/task_4.php
@@ -0,0 +1,14 @@
+<?php
+
+
+
+class MyClass extends ParentClass implements InterfaceOne {
+    private static $instance;
+    
+    private function __construct() {}
+    
+    public static function getInstance() {
+        if(!self::$instance) self::$instance = new self();
+        return self::$instance;
+    }
+}
